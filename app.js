fca146afd55058915a0a44f94000724ee1edea5a
tebeck <tebeckford@gmail.com>
2017-11-21 00:59:29 -0300

    App detail change, css border

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var items = require('./routes/items');
-var detail = require('./routes/detail');
+
 
 var app = express();
 
@@ -25,7 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/items', items);
-app.use('/detail', detail);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
