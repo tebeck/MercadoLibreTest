5f5c8ee681b45128d73b74a30180357eecd146b6
tebeck <tebeckford@gmail.com>
2017-11-22 11:27:54 -0300

    Currency, Font, Readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# MercadoLibreTest
+# MercadoLibre Front End Test
+
+El proyecto se basa en un armado de tres vistas utilizando API de MercadoLibre.
+
+## Getting Started
+
+git clone https://github.com/tebeck/MercadoLibreTest.git
+
+### Prerequisites
+
+```
+NPM
+NODEJS
+EXPRESS
+```
+
+### Installing
+
+First step
+
+```
+npm install
+```
+
+```
+npm start
+```
+
+## Built With
+
+Front End:
+- JADE
+- CSS3 ( Bootstrap + Own Stylesheet )
+
+Back End:
+- NODEJS v8.9.1
+- EXPRESS
+
+
+
--- routes/items.js
+++ routes/items.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var request = require('request');
+var currencyFormatter = require('currency-formatter');
 var router = express.Router();
 
 
@@ -9,6 +10,17 @@ router.get('/', function(req, res, next) {
   var search = req.query.search;
   request('https://api.mercadolibre.com/sites/MLA/search?q='+search, function (error, response, body) {
     var sApi = JSON.parse(body);
+
+    for (var i = 0, len = sApi.results.length; i < len; i++) {
+      sApi.results[i].price = currencyFormatter.format(sApi.results[i].price, {
+        symbol: '$',
+        decimal: '*',
+        thousand: '.',
+        precision: 0,
+        format: '%s %v'
+      });
+    }
+
     if (!error && response.statusCode == 200) {
         res.render('items', { title: "MercadoLibre", sApi });
       }
@@ -27,8 +39,16 @@ router.get('/:id', function(req, res, next) {
       request('https://api.mercadolibre.com/categories/'+id_category, function(error, response, body){
             var cApi = JSON.parse(body);
 
+      var price = currencyFormatter.format(iApi.price, {
+        symbol: '$',
+        decimal: '*',
+        thousand: '.',
+        precision: 0,
+        format: '%s %v' // %s is the symbol and %v is the value
+    });
+
   if (!error && response.statusCode == 200) {
-        res.render('detail', { title: "MercadoLibre", iApi, dApi, usado, nuevo, cApi });
+        res.render('detail', { title: "MercadoLibre", iApi, dApi, usado, nuevo, cApi, price });
       }
     });
   });
