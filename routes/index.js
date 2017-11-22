6e9083e764f5efb7763459fbe94f5086dd7b34b3
tebeck <tebeckford@gmail.com>
2017-11-22 01:02:16 -0300

    Css

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ var request = require('request');
 
 /* GET search page. */
 router.get('/', function(req, res, next) {
-  var search = req.query.search; // $_GET["search"] Superglobal.
+// $_GET["search"] Superglobal.
+  var search = req.query.search;
 
   request('https://api.mercadolibre.com/sites/MLA/search?q='+search, function (error, response, body) {
+    var sApi = JSON.parse(body);
       if (!error && response.statusCode == 200) {
-          res.render('index', { title: "MercadoLibre", resultados: JSON.parse(body) });
+          res.render('index', { title: "MercadoLibre", sApi });
        }
   });
 });
--- routes/items.js
+++ routes/items.js
@@ -1,37 +1,39 @@
 var express = require('express');
-var router = express.Router();
 var request = require('request');
+var router = express.Router();
+
 
 /* GET search page. */
 router.get('/', function(req, res, next) {
-  var search = req.query.search; // $_GET["search"] Superglobal.
-
+ // $_GET["search"] Superglobal.
+  var search = req.query.search;
   request('https://api.mercadolibre.com/sites/MLA/search?q='+search, function (error, response, body) {
-
-      if (!error && response.statusCode == 200) {
-          res.render('items', { title: "MercadoLibre",resultados: JSON.parse(body) });
-       }
+    var sApi = JSON.parse(body);
+    if (!error && response.statusCode == 200) {
+        res.render('items', { title: "MercadoLibre", sApi });
+      }
   });
 });
-
 
 router.get('/:id', function(req, res, next) {
+    var id = req.params.id;
+    var nuevo = "Nuevo";
+    var usado = "Usado";
+  request('https://api.mercadolibre.com/items/'+id+'/description', function(error, response, body){
+      var dApi = JSON.parse(body);
+    request('https://api.mercadolibre.com/items/'+id, function (error, response, body) {
+        var iApi = JSON.parse(body);
+        var id_category = iApi.category_id;
+      request('https://api.mercadolibre.com/categories/'+id_category, function(error, response, body){
+            var cApi = JSON.parse(body);
 
-var id = req.params.id;
-var nuevo = "Nuevo";
-var usado = "Usado";
-
-request('https://api.mercadolibre.com/items/'+id+'/description', function(error, response, body){
-
-  var desc= JSON.parse(body);
-
-  request('https://api.mercadolibre.com/items/'+id, function (error, response, body) {
-
-    if (!error && response.statusCode == 200) {
-        res.render('detail', { title: "MercadoLibre",resultados: JSON.parse(body), desc, usado, nuevo });
-     }
-   });
+  if (!error && response.statusCode == 200) {
+        res.render('detail', { title: "MercadoLibre", iApi, dApi, usado, nuevo, cApi });
+      }
+    });
   });
 });
+});
+
 
 module.exports = router;
